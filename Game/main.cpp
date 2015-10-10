4eb4183c30db7d76b47744944f70d569e023540c
HAAKS <heba.salama18@gmail.com>
2015-10-10 19:47:59 +0200

    Initial Commit

---
+++ Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Game
+//
+//  Created by Heba A. Salama on 10/10/15.
+//  Copyright (c) 2015 Heba A. Salama. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
